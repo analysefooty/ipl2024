e2eea8ce333c56fa24783f58e28fea6479761c85
Ravi Ramineni <ravi@srcftbl.com>
2024-08-19 08:25:40 -0500

    Merge remote-tracking branch 'origin/main'

--- pages/player_batting_radar.py
+++ pages/player_batting_radar.py
@@ -33,12 +33,15 @@ conn = connect(
 # teams query
 cursor = conn.cursor()
 
-batsman_list_query = (
-    f"select batsman_name from ipl group by batsman_name having count(distinct id) > 60"
-)
+batsman_list_query = f"select batsman_name from ipl group by batsman_name having count(distinct id) > 60 order by BATSMAN_NAME"
 cursor.execute(batsman_list_query)  # .to_pandas()
 batsman_df = cursor.fetch_pandas_all()
 
+batsman_avg_balls_faced_query = (
+    f"select batsman_name, batsman_balls_faced_avg_pct from batsman_avg_balls_faced_pct"
+)
+cursor.execute(batsman_avg_balls_faced_query)  # .to_pandas()
+batsman_avg_balls_faced_df = cursor.fetch_pandas_all()
 # Write directly to the app
 st.title("IPL 2024 Data - Batting Radar")
 st.text(
@@ -54,7 +57,7 @@ if len(batsmen_selected) != 2:
 if submitted:
     batsmen_list = "','".join(batsmen_selected)
 
-    batsman_percentile_query = f"SELECT batsman_name, match_phase, sr_percentile FROM AGG_RUNS_BY_MATCH_PHASE WHERE batsman_name IN ('{batsmen_list}')"
+    batsman_percentile_query = f"SELECT batsman_name, match_phase, sr_percentile*100 AS sr_percentile FROM AGG_RUNS_BY_MATCH_PHASE WHERE batsman_name IN ('{batsmen_list}')"
     cursor.execute(batsman_percentile_query)  # .to_pandas()
     percentile_data_df = cursor.fetch_pandas_all()
     # st.text(percentile_data_df.values.tolist())
@@ -62,20 +65,29 @@ if submitted:
         subset=["MATCH_PHASE"]
     )
     categories = percentile_data_df_unique["MATCH_PHASE"].values.tolist()
+    # categories.append("BALLS FACED")
     values = percentile_data_df["SR_PERCENTILE"].values.tolist()
+
+    # st.text(values)
 
     def get_player_data(player_name, df):
         player_data = []
+
         for category in categories:
             value = df[
                 (df["BATSMAN_NAME"] == player_name) & (df["MATCH_PHASE"] == category)
             ]["SR_PERCENTILE"].values
             player_data.append(value[0] if len(value) > 0 else 0)
+        player_data.append(
+            batsman_avg_balls_faced_df[
+                batsman_avg_balls_faced_df["BATSMAN_NAME"] == player_name
+            ]["BATSMAN_BALLS_FACED_AVG_PCT"].values[0]
+        )
         return player_data
 
     player1_data = get_player_data(batsmen_selected[0], percentile_data_df)
     player2_data = get_player_data(batsmen_selected[1], percentile_data_df)
-
+    categories.append("BALLS FACED")  # Add the balls faced category to the list
     # Add the first value to the end to close the plot
     player1_data.append(player1_data[0])
     player2_data.append(player2_data[0])
@@ -84,6 +96,7 @@ if submitted:
 
     num_vars = len(values)  # Set num_vars to the length of your data
     num_categories = len(categories)
+
     # Generate angles based on the number of variables
     angles = np.linspace(0, 2 * np.pi, num_categories, endpoint=False).tolist()
     values += values[:1]
@@ -114,7 +127,7 @@ if submitted:
     # Set the labels
     ax.set_xticks(angles[:-1])
     ax.set_xticklabels(categories, color="blue", size=10)
-    ax.set_yticks([0.25, 0.50, 0.75, 1.0])
+    ax.set_yticks([25, 50, 75, 100])
     # ax.set_yticklabels(values, color="grey", size=7)
     ax.legend(loc="upper right", bbox_to_anchor=(0.1, 0.1))
 
